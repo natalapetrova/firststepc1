7c6d3557ebf665d00bb54050b62249f90274d7fa
Natali Petrova <natalapetrova@bk.ru>
2022-07-13 16:44:07 +0300

    add solution for task 0

---
+++ solutiontask/Program.cs
@@ -0,0 +1,19 @@
+﻿// Первый вариант решения задачи
+// string? inputLine = Console.ReadLine();
+
+// if(inputLine != null)
+// {
+//     int inputNumber = int.Parse(inputLine);
+//     int outNumber = inputNumber*inputNumber;
+//     Console.WriteLine(outNumber);
+// }
+
+// //Второй вариант решения задачи
+// string? inputLine = Console.ReadLine();
+
+// if(inputLine != null)
+// {
+//     int inputNumber = int.Parse(inputLine);
+//     int outNumber = (int)Math.Pow(inputNumber,2);
+//     Console.WriteLine(outNumber);
+// }
