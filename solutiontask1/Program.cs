7d13c3ac0089f42318593641ea4c0172a636a24b
Natali Petrova <natalapetrova@bk.ru>
2022-07-14 11:31:34 +0300

    add solution task 1 and task 2

---
+++ solutiontask1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Первый вариант решения
+// string? inputLineOne = Console.ReadLine();
+// string? inputLineTwo = Console.ReadLine();
+
+// if(inputLineOne != null && inputLineTwo != null){
+//     int inputNumberOne = int.Parse(inputLineOne);
+//     int inputNumberTwo = int.Parse(inputLineTwo);
+
+
+//     if(inputNumberOne == inputNumberTwo * inputNumberTwo)
+//     {
+//         Console.WriteLine("yes");
+//     }
+//     else
+//     {
+//         Console.WriteLine("no");
+
+//     }
+// }
+
+//Второй вариант решения
+// string? inputLineOne = Console.ReadLine();
+// string? inputLineTwo = Console.ReadLine();
+
+// if(inputLineOne != null && inputLineTwo != null){
+//     int inputNumberOne = int.Parse(inputLineOne);
+//     int inputNumberTwo = int.Parse(inputLineTwo);
+
+
+//     if(inputNumberTwo == Math.Sqrt(inputNumberOne))
+//     {
+//         Console.WriteLine("yes");
+//     }
+//     else
+//     {
+//         Console.WriteLine("no");
+
+//     }
+// }
