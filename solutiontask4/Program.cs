9916081ffb6acc03bdb2ac4810dafc909ceb9a15
Natali Petrova <natalapetrova@bk.ru>
2022-07-14 12:33:51 +0300

    add solution task 4

---
+++ solutiontask4/Program.cs
@@ -0,0 +1,29 @@
+﻿//Первое решение
+// string? inputLine = Console.ReadLine();
+
+// if(inputLine != null)
+// {
+//     int inputNumber = int.Parse(inputLine);
+
+//     int outputNumber = inputNumber%10;
+
+//     Console.WriteLine(outputNumber);
+// }
+
+//Второе решение
+// string? inputLineOne = Console.ReadLine();
+
+// if(inputLineOne != null)
+// {
+//     string lastNum = inputLineOne.Substring(2,1);
+//     Console.WriteLine(lastNum);
+// }
+
+//Третий вариант решения
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    char[] M = inputLine.ToCharArray();
+    Console.WriteLine(M[2]);
+}
