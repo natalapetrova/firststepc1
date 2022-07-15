2563203e600d85133edd9d6d5e8b76a0819aca66
Natali Petrova <natalapetrova@bk.ru>
2022-07-15 19:51:13 +0300

    add solution task 9

---
+++ solutiontask9/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Random numberSintezator = new Random();
+int number = numberSintezator.Next(10,100);
+
+    Console.WriteLine(number);
+
+
+int firstNumber = number/10;
+int secondNumber = number%10;
+
+if(firstNumber > secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+
+else
+{
+    Console.WriteLine(secondNumber);
+
+}
