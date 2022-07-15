86a3ea5e7c0d3b6ff76720911c0993e20af2c16d
Natali Petrova <natalapetrova@bk.ru>
2022-07-15 13:43:20 +0300

    add solution task 5

---
+++ solutiontask3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Первый варинат решения
+// string? inputLineOne = Console.ReadLine();
+
+// if(inputLineOne != null)
+// {
+//     int inputNumber = int.Parse(inputLineOne);
+
+//     int startNumber = (inputNumber * -1);
+
+//     string lineOutput = "";
+
+//     while (startNumber < inputNumber)
+//     {
+//         lineOutput = lineOutput+startNumber+", ";
+//         startNumber++;
+//     }
+
+//     Console.WriteLine(lineOutput);
+// }
+
+// Второй вариант решения
+// string? inputLineOne = Console.ReadLine();
+
+// if(inputLineOne != null)
+// {
+//     int inputNumber = int.Parse(inputLineOne);
+
+//     int startNumber = 1;
+
+//     string lineOutput = "0";
+
+//     while (startNumber <= inputNumber)
+//     {
+//         lineOutput = startNumber*(-1)+", "+lineOutput+", "+startNumber;
+//         startNumber++;
+//     }
+
+//     Console.WriteLine(lineOutput);
+// } 
---
+++ solutiontask5/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая на вход принммает два числа и выдает, какое число больше, а какое меньше.
+//
+string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+
+if(inputLineOne != null && inputLineTwo != null)
+{
+    int inputNumberOne = int.Parse(inputLineOne);
+    int inputNumberTwo = int.Parse(inputLineTwo);
+    
+    if(inputNumberOne > inputNumberTwo)
+    {
+        Console.Write("max = ");
+        Console.Write(inputNumberOne);
+        Console.Write(" min = ");
+        Console.Write(inputNumberTwo);
+    }
+    else
+    {
+        Console.Write("max = ");
+        Console.Write(inputNumberTwo);
+        Console.Write(" min = ");
+        Console.Write(inputNumberOne);
+    }
+}
