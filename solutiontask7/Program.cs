04b6ef3f26ea08342ef809d07610569ea367bd85
Natali Petrova <natalapetrova@bk.ru>
2022-07-15 15:17:18 +0300

    add solution task 7

---
+++ solutiontask7/Program.cs
@@ -0,0 +1,15 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+
+    if(inputNumber%2 == 0)
+    {
+        Console.Write("Yes");
+    }
+    else
+    {
+        Console.Write("No");
+    }
+}
