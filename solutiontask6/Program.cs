311a0413f349a56548b435d89737f5051b40be25
Natali Petrova <natalapetrova@bk.ru>
2022-07-15 13:55:33 +0300

    add solution task 6

---
+++ solutiontask6/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая на вход принммает три числа и выдает максимальное из этих чисел.
+//
+string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+string? inputLineThree = Console.ReadLine();
+
+if(inputLineOne != null && inputLineTwo != null && inputLineThree != null)
+{
+    int inputNumberOne = int.Parse(inputLineOne);
+    int inputNumberTwo = int.Parse(inputLineTwo);
+    int inputNumberThree = int.Parse(inputLineThree);
+    
+    if(inputNumberOne > inputNumberTwo)
+    {
+        if(inputNumberOne > inputNumberThree)
+        {
+            Console.Write(inputNumberOne);
+        }
+        else
+        {
+            Console.Write(inputNumberThree);
+        }
+    }
+    else
+    {
+        if(inputNumberTwo > inputNumberThree)
+        {
+            Console.Write(inputNumberTwo);
+        }
+        else
+        {
+            Console.Write(inputNumberThree);
+        }
+    }
+}
