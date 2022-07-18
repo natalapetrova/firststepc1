d26c76a6c5f0bf96b8fcf07bb143caa8d6d96609
Natali Petrova <natalapetrova@bk.ru>
2022-07-18 18:26:43 +0300

    add solution for task 13

---
+++ solutiontask13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число");
+string? inputFirst = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string? inputSecond = Console.ReadLine();
+
+if ((inputFirst != null) && (inputFirst != null))
+{
+    int inputNumberFirst = int.Parse(inputFirst);
+    int inputNumberSecond = int.Parse(inputSecond);
+    int a = inputNumberFirst * inputNumberFirst;
+    int b = inputNumberSecond * inputNumberSecond;
+
+    if ((a == inputNumberSecond) || (b == inputNumberFirst))
+    {
+        Console.WriteLine("Да");
+
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+        
+    }
+}
