eb541664f609219492824e887515f7062a38323b
Natali Petrova <natalapetrova@bk.ru>
2022-07-18 16:32:49 +0300

    add solution for task 10

---
+++ solutiontask10/Program.cs
@@ -0,0 +1,35 @@
+﻿//Первый способ решения
+// int number = new Random().Next(100,1000);
+// Console.WriteLine("число " + number);
+// int numberSecondDigit = number/100;
+// int numberThirDigit = number%10;
+// Console.WriteLine("Число после удаления второй цифры: " + numberSecondDigit + numberThirDigit);
+
+//Второй способ решения
+
+// void middleDigitCutter()
+// {
+//     int number = new Random().Next(100,1000);
+//     Console.Write("The random number is :");
+//     Console.WriteLine(number);
+//     string stringNum = number.ToString();
+//     Console.Write("The modified num is :");
+//     Console.Write(stringNum[0]);
+//     Console.WriteLine(stringNum[2]);
+    
+// }
+// middleDigitCutter();
+
+
+void middleDigitCutter2()
+{
+    int number = new Random().Next(100,1000);
+    Console.WriteLine(number);
+    int num1 = number / 100;
+    int num2 = number % 10;
+
+    Console.Write("The modified num is :");
+    Console.Write(num1 * 10 + num2);
+    
+}
+middleDigitCutter2();
