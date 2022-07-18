be8b61878db4bebe7e5ce00c48d7524e259f449c
Natali Petrova <natalapetrova@bk.ru>
2022-07-18 16:58:43 +0300

    add solution for task 11

---
+++ solutiontask11/Program.cs
@@ -0,0 +1,37 @@
+﻿//Первое решение
+// string? inputLineOne = Console.ReadLine();
+// string? inputLineTwo = Console.ReadLine();
+
+// if(inputLineOne != null && inputLineTwo != null)
+// {
+//     int inputNumberOne = int.Parse(inputLineOne);
+//     int inputNumberTwo = int.Parse(inputLineTwo);
+
+//     int div = inputNumberTwo%inputNumberOne;
+//     if(div == 0)
+//     {
+//         Console.WriteLine("yes");
+//     }
+//     else
+//     {
+//         Console.WriteLine(div);
+
+//     }
+// }
+
+//Второе решение
+Console.Write("Enter first number: ");
+string? firstLine = Console.ReadLine();
+Console.Write("Enter second number: ");
+string? secondLine = Console.ReadLine();
+int first = int.Parse(firstLine);
+int second = int.Parse(secondLine);
+
+if(second%first == 0)
+{
+        Console.WriteLine("Кратное");
+}
+else
+{
+        Console.WriteLine("Не кратное, " + second%first);
+}
