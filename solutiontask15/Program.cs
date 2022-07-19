454fa57cae6a195d2101c5f154a5a4def3d6871b
Natali Petrova <natalapetrova@bk.ru>
2022-07-19 13:19:15 +0300

    add solution for task 15

---
+++ solutiontask15/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число");
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    char[] numberArray = inputLine.ToCharArray();
+    int inputNumber = int.Parse(inputLine);
+
+    if (inputNumber > 99 )
+        {
+           Console.WriteLine(numberArray[2]);
+        }
+    else
+    {
+           Console.WriteLine("Третьей цифры нет");
+
+    }
+}
