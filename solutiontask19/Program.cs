dd13fbbf6410b94af10dfa4538a24963f088dbc1
Natali Petrova <natalapetrova@bk.ru>
2022-07-22 17:59:01 +0300

    add solution for tsak 20

---
+++ solutiontask19/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите 5-ти значное число");
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    char[] numberArray = inputLine.ToCharArray();
+    if (numberArray.Length == 5)
+    {
+        if ((numberArray[0] == numberArray[4]) && (numberArray[1] == numberArray[3]))
+        {
+            Console.WriteLine("Число палиндром");
+        }
+        else
+        {
+            Console.WriteLine("Число не палиндром");
+        }
+    }
+
+    else
+        {
+            Console.WriteLine("Число не пятизначное");
+        }
+}
