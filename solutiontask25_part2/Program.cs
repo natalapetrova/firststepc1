1742cb31595fe5d3bce9fd1b397dcd1038758882
Natali Petrova <natalapetrova@bk.ru>
2022-08-05 19:02:20 +0300

    add solution for task 25 part 2

---
+++ solutiontask25_part2/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.Clear();
+Console.WriteLine("Введите операцию используя один из знаков + - / * ^");
+Console.WriteLine("В данном формате используйте целые числа");
+
+void Calculate(string[] getNum)
+{
+    double result;
+
+    switch (getNum[1])
+    {
+        case "+":
+            result = int.Parse(getNum[0]) + int.Parse(getNum[2]);
+            Console.WriteLine(getNum[0] + " " + getNum[1] + " " + getNum[2] + " = " + result);
+            break;
+
+        case "-":
+            result = int.Parse(getNum[0]) - int.Parse(getNum[2]);
+            Console.WriteLine(getNum[0] + " " + getNum[1] + " " + getNum[2] + " = " + result);
+            break;
+
+        case "*":
+            result = int.Parse(getNum[0]) * int.Parse(getNum[2]);
+            Console.WriteLine(getNum[0] + " " + getNum[1] + " " + getNum[2] + " = " + result);
+            break;
+
+        case "/":
+            result = int.Parse(getNum[0]) / int.Parse(getNum[2]);
+            Console.WriteLine(getNum[0] + " " + getNum[1] + " " + getNum[2] + " = " + result);
+            break;
+
+        case "^":
+            result = Math.Pow(int.Parse(getNum[0]), int.Parse(getNum[2]));
+            Console.WriteLine(getNum[0] + " " + getNum[1] + " " + getNum[2] + " = " + result);
+            break;
+
+        default:
+        Console.Write("Введен неверный формат");
+        break;
+    }
+}
+
+try
+{
+    string[] operation = Console.ReadLine().Split(" ");
+
+    Calculate(operation);
+}
+catch
+{
+
+    Console.WriteLine("");
+
+}
