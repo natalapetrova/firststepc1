29d8fef2100dbc3f738c9a7f5bd7dc20927e6f37
Natali Petrova <natalapetrova@bk.ru>
2022-08-18 16:02:05 +0300

    add solution for task 37

---
+++ solutiontask37/Program.cs
@@ -0,0 +1,51 @@
+﻿
+int[] FillingArray()
+{
+    // обязательно вызываем new чтобы выделить память для массива
+    int[] outArray = new int[123];                               //Выходной массив
+
+    int i = 0;                                                  // Переменный оператор
+
+    System.Random numberSintezator = new System.Random();       // Экземпляр класса который создает рандомные номера
+
+    while (i < 123)
+    {
+        // Получаем новое значение
+        outArray[i] = numberSintezator.Next(-1000, 1000);
+        // Увеличиваем инкремент
+        i++;
+    }
+    // Возвращаем значение
+    return outArray;
+}
+
+
+void PrintIntArray(int[] inputArray)
+{
+    int i = 0;
+
+    while (i < inputArray.Length - 1)
+    {
+        Console.Write(inputArray[i] + ",");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+int[] ColculateTask(int[] inputArray)
+{
+    int[] resultArray = new int[inputArray.Length / 2 + 1];
+    int i = 0;
+
+    while (i < resultArray.Length)
+    {
+        resultArray[i] = inputArray[i] * inputArray[inputArray.Length - 1 - i];
+        i++;
+    }
+    return resultArray;
+}
+
+int[] bufferArray = FillingArray();
+PrintIntArray(bufferArray);
+int[] resultArray = ColculateTask(bufferArray);
+PrintIntArray(resultArray);
