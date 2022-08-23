8a2a15d72c6b281549fefc7121227b1401be0f24
Natali Petrova <natalapetrova@bk.ru>
2022-08-23 15:42:17 +0300

    add solution for task 45

---
+++ solutiontask45/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая будет создавать копию заданного 
+// одномерного массива с помощью поэлементного копирования.
+//Метод возвращает массив заполненый случайными числами 
+int[] FillingArray()
+{
+    int[] outArray = new int[12];
+    int i = 0;
+    System.Random numberSintezator = new System.Random();
+    while (i < 12)
+    {
+        outArray[i] = numberSintezator.Next(-1000,1000);
+        i++;
+    }
+    return outArray;
+}
+
+//Метод печатает массив
+void PrintIntArray(int[] inputArray)
+{
+    //Буфферная переменная
+    int i = 0;
+    //Пробегаем все элементы массива
+    while (i < inputArray.Length - 1)
+    {
+        //Выводим элемент массива
+        Console.Write(inputArray[i] + ", ");
+        //Увеличиваем инкремент
+        i++;
+    }
+    //Выводим элемент массива
+    Console.WriteLine(inputArray[i]);
+}
+
+int[] CopyArrayStandartTool( int[] inputArray)
+{
+    int[] buferArray = new int[inputArray.Length];
+    inputArray.CopyTo(buferArray,0);
+    return buferArray;
+}
+
+int[] testArray = FillingArray();
+PrintIntArray(testArray);
+
+int[] outArray = (int[])CopyArrayStandartTool(testArray);
+PrintIntArray(outArray);
