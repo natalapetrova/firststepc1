57cb2e48faeafdf7f669a836b007fdb8d068e68c
Natali Petrova <natalapetrova@bk.ru>
2022-08-29 17:53:51 +0300

    add solution for task 53

---
+++ solutiontask53/Program.cs
@@ -0,0 +1,69 @@
+﻿//=========================================================================================================
+// Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива. 
+// Разбор на семинаре
+//=========================================================================================================
+
+// метод для заполнения двумерного массива
+int[,] FillTwoDimArray(int countRow, int countColumn)
+{
+    System.Random numberSyntezator = new System.Random();
+    int i = 0; int j = 0;
+    int[,] outArray = new int[countRow, countColumn];
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i, j] = numberSyntezator.Next(0, 101);
+            j++;
+        }
+        i++;
+    }
+    return outArray;
+}
+
+void PrintTwoDimArray(int[,] inputArray)
+{
+    int i = 0; int j = 0;
+
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.Write(inputArray[i, j] + "\t");
+            j++;
+        }
+        //Console.Write("\n");
+        Console.WriteLine();
+        i++;
+    }
+}
+
+// Метод меняет местами первую ипоследнюю строчку
+// Принимает двумерный массив
+// Возвращает модефицированный входной массив
+int[,] RotateRowFirstLast(int[,] inputArray)
+{
+    int bufferElement = 0;
+    int i = 0; int j = 0;
+    while (j < inputArray.GetLength(1))
+    {
+        //Блок ротации элементов в столбце
+        bufferElement = inputArray[0, j];
+        inputArray[0, j] = inputArray[inputArray.GetLength(0) - 1, j];
+        inputArray[inputArray.GetLength(0) - 1, j] = bufferElement;
+        j++;
+    }
+    return inputArray;
+}
+
+
+ int[,] twoDimArray = FillTwoDimArray(3, 4);
+ PrintTwoDimArray(twoDimArray);
+
+ Console.WriteLine();
+ //----------------------------------------------------------
+ PrintTwoDimArray(RotateRowFirstLast(twoDimArray));
+ //----------------------------------------------------------
