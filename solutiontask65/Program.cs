c9181fa82cc0614fbcf3291819914647303a7360
Natali Petrova <natalapetrova@bk.ru>
2022-09-05 12:24:47 +0300

    add solution for task 65

---
+++ solutiontask65/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Clear();
+int numberM = ReadData("Задайте число M");
+int numberN = ReadData("Задайте число N");
+
+if (numberM < numberN)
+{
+    NaturalNamberPrinter(numberM, numberN);
+}
+else
+{
+    NaturalNamberPrinter(numberN, numberM);
+}
+
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+// 3 6
+// 4 6
+// 5 6
+// 6 6
+// 7 6
+int sum =0;
+
+void NaturalNamberPrinter(int m, int n)
+{
+    if (m - 1 == n) return;
+    Console.Write(m + " ");
+    NaturalNamberPrinter(m + 1, n);
+}
